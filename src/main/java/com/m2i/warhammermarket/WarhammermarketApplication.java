5d19d888df2c61dd96ea078f29634756c4c4d260
Cecile <cecile.baillard@laposte.net>
2021-10-21 16:06:12 +0200

    Project skeleton made with Spring Initializr

---
+++ src/main/java/com/m2i/warhammermarket/WarhammermarketApplication.java
@@ -0,0 +1,13 @@
+package com.m2i.warhammermarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarhammermarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarhammermarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/m2i/warhammermarket/WarhammermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2i.warhammermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarhammermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
