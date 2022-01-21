a2cd7fc5af8c20b435136e9ab7bb066a51b1955b
Malek MOKRANI <malek.mokrani@outlook.com>
2022-01-21 10:14:07 +0100

    Feature/23 : Add getter setter for handling update

--- src/main/java/com/m2i/warhammermarket/entity/wrapper/ProfileWrapper.java
+++ src/main/java/com/m2i/warhammermarket/entity/wrapper/ProfileWrapper.java
@@ -30,11 +30,41 @@ public class ProfileWrapper {
         phone = user.getPhone();
         mail = user.getUser().getMail();
         birthdate = user.getBirthdate();
-        number = address.getNumber();
-        street = address.getStreet();
-        additionalAddress = address.getAdditionalAddress();
-        postalCode = address.getPostalCode();
-        city = address.getCity();
-        country = address.getCountry();
+        
+        if(address!=null)
+        {
+        	setAddressValues(address);
+        }else {
+        	number = "666";
+        	street = "rue des Adresses Null";
+        	additionalAddress = "Bâtiment C";
+        	postalCode = "59160";
+        	city = "Lille";
+        	country = "France";        	
+        }
     }
+    
+
+    
+    public void setAddressValues(AddressDAO address)
+    {
+    	number = address.getNumber();
+    	street = address.getStreet();
+    	additionalAddress = address.getAdditionalAddress();
+    	postalCode = address.getPostalCode();
+    	city = address.getCity();
+    	country = address.getCountry();        	
+    }
+
+	public UsersInformationDAO getUserInfo() {
+		// TODO Auto-generated method stub
+		return new UsersInformationDAO(null, lastName, firstName, phone, birthdate, null, null);
+	}
+
+
+
+	public AddressDAO getAddress() {
+		
+		return new AddressDAO(null, number, street, additionalAddress, postalCode, city, country, null);
+	}
 }
