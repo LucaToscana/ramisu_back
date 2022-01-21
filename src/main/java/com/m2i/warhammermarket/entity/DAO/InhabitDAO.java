b0683ebe40d7e5c137ea11d55b6d36f53c4c3f0f
Malek MOKRANI <malek.mokrani@outlook.com>
2022-01-21 10:04:05 +0100

    Feature/23 : Add table mapping

--- src/main/java/com/m2i/warhammermarket/entity/DAO/InhabitDAO.java
+++ src/main/java/com/m2i/warhammermarket/entity/DAO/InhabitDAO.java
@@ -5,6 +5,10 @@ import lombok.*;
 import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
 import javax.persistence.Table;
 import java.io.Serializable;
 
@@ -21,4 +25,16 @@ public class InhabitDAO implements Serializable {
 
     @Column (name = "is_main")
     private int isMain;
+    
+    
+    
+    @ManyToOne(fetch = FetchType.EAGER)
+    @MapsId("address")
+    @JoinColumn(name = "id_address")
+    AddressDAO address;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @MapsId("user")
+    @JoinColumn(name = "id_users")
+    UsersInformationDAO user;
 }
