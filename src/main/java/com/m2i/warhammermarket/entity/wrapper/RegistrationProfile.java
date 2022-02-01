8dfc640fa1b2750c4d76a2760fa1c13afc5592b4
Malek MOKRANI <malek.mokrani@outlook.com>
2022-02-01 09:12:07 +0100

    Add ProfileWrapper subclass for user registration

---
+++ src/main/java/com/m2i/warhammermarket/entity/wrapper/RegistrationProfile.java
@@ -0,0 +1,33 @@
+package com.m2i.warhammermarket.entity.wrapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.m2i.warhammermarket.entity.DTO.UserSecurityDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class RegistrationProfile extends ProfileWrapper {
+	private String password;
+
+	public UserSecurityDTO getUserSecurity() {
+		 
+		List<String> authorities = new ArrayList<String>();
+		 authorities.add("user");
+	        
+		UserSecurityDTO user = new UserSecurityDTO();
+						user.setAuthorities(authorities);
+						user.setMail(this.getMail());
+						user.setPassword(this.getPassword());
+		return user;
+	}
+}
