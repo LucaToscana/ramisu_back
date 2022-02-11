5aa6119d099dea415264ac7cd5d49dd73b6a8c4f
Malek Mokrani <malek.mokrani@outlook.com>
2022-02-11 17:24:37 +0100

    throw exception if account inactive

--- src/main/java/com/m2i/warhammermarket/service/implement/JwtUserDetailService.java
+++ src/main/java/com/m2i/warhammermarket/service/implement/JwtUserDetailService.java
@@ -1,15 +1,10 @@
 package com.m2i.warhammermarket.service.implement;
 
 import com.m2i.warhammermarket.repository.UserRepository;
-
-
+import com.m2i.warhammermarket.controller.exception.InactiveAccountException;
 import com.m2i.warhammermarket.entity.DAO.UserDAO;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -26,7 +21,11 @@ public class JwtUserDetailService implements UserDetailsService {
     public UserDetails loadUserByUsername(String mail) throws UsernameNotFoundException {
     	
         UserDAO userDAO = userRepository.findByMail(mail);
-        if(userDAO == null) throw new UsernameNotFoundException("User not found with this mail : " + mail);
+        
+    	if(userDAO == null) 	throw new UsernameNotFoundException("User not found with this mail : " + mail);
+    
+    	if(!userDAO.isActive())	throw new RuntimeException("");
+        
         return new User(userDAO.getMail(), userDAO.getPassword(), userDAO.getAuthorities());
     }
    
