c74d34086b3a4cfb046f39dd9cfefa45bcf86b48
Malek MOKRANI <malek.mokrani@outlook.com>
2022-04-13 10:42:46 +0200

    Set index path /

--- src/main/java/com/m2i/warhammermarket/controller/ThymeleafPagesController.java
+++ src/main/java/com/m2i/warhammermarket/controller/ThymeleafPagesController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ThymeleafPagesController {
 
 
-    @GetMapping("/index.html")
+    @GetMapping("/")
     public String index() {
         return "index";
     }
